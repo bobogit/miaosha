334f376ef86bc1d6f6960a99990d4b10ef9c9d71
wangbo <804623282@qq.com>
2019-08-22 17:56:38 +0800

    init

---
+++ src/main/java/com/miaoshaproject/App.java
@@ -0,0 +1,15 @@
+package com.miaoshaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Hello world!
+ */
+@EnableAutoConfiguration
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
+    }
+}
